8fbd8f1d09b3cccce5af2bab4bdd4e5853736063
MashMinners <Valehtelit@mail.ru>
2025-11-26 11:07:50 +1000

    #Отправка в БД отзыва

--- src/Application/ReviewCollector/Models/Review.php
+++ src/Application/ReviewCollector/Models/Review.php
@@ -7,8 +7,19 @@ use Engine\DTO\BaseDTO;
 class Review extends BaseDTO implements \JsonSerializable
 {
     protected string|null $reviewId;
-    protected string|null $reviewType;
+    protected string|null $reviewEmployeeId;
+    protected int|null $reviewStatus;
+    protected string|null $reviewText;
+    protected string|null $reviewDate;
     public function __construct(string $json){
+        /**
+         * Заполнить все свойства null
+         */
+        $properties = get_class_vars(self::class);
+        foreach ($properties as $name => $value){
+            $this->$name = $value;
+        }
+        //Проинициализировать значениями
         $this->init($json);
     }
 
@@ -16,7 +27,7 @@ class Review extends BaseDTO implements \JsonSerializable
         return $this->$name;
     }
 
-    public function jsonSerialize()
+    public function jsonSerialize() : mixed
     {
         $properties = get_object_vars($this);
         return $properties;
--- src/Application/ReviewCollector/Models/ReviewCollector.php
+++ src/Application/ReviewCollector/Models/ReviewCollector.php
@@ -14,16 +14,37 @@ class ReviewCollector
     }
 
     /**
+     * Выбрать отзывы за период
+     * @return void
+     */
+    public function getByDate(){
+
+    }
+
+    /**
+     * Выбирает отзывы по пользователю за 30 дней
+     * @return void
+     */
+    public function getByEmployee(){
+        $query = ("SELECT * FROM reviews WHERE reviews_employee_id = :id AND ");
+
+    }
+
+    /**
      * Вставляет в базу данных отзыв
      * @param string $type - good/bad
      * @return void
      */
     public function insert(Review $review) : string{
-        $query = (""); //Insert Query
+        $query = ("INSERT INTO pulse.reviews (reviews_review_id, reviews_employee_id, reviews_review_status, reviews_review_text, reviews_review_date) 
+                   VALUES (:reviewId, :employeeId, :reviewStatus, :reviewText, :reviewDate)"); //Insert Query
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([
             'reviewId' => $reviewId = Uuid::uuid4()->toString(),
-            'reviewType' => $review->type
+            'employeeId'=> $review->reviewEmployeeId,
+            'reviewStatus' => $review->reviewStatus,
+            'reviewText' => $review->reviewText,
+            'reviewDate' => strtotime($review->reviewDate)
         ]);
         return $reviewId;
     }
