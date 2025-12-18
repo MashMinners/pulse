d7616f186f7f3493c2de4d638d901e5dd64105db
Rain <Valehtelit@mail.ru>
2025-12-18 21:01:40 +1000

    #Structured response

---
+++ src/Engine/DTO/StructuredResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Engine\DTO;
+
+class StructuredResponse
+{
+    public function __construct(private $code = 204, private $message = null, private $body = []){
+
+    }
+
+    public function __get($name){
+        return $this->$name;
+    }
+    public function __set($name, $value){
+        $this->$name = $value;
+    }
+
+    public function setBody($key, $value){
+        $this->body[$key] = $value;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'Code' => $this->code,
+            'Message' => $this->message,
+            'Body' => $this->body
+        ];
+    }
+
+}
