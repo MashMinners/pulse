b4389058ccdc68b958b0f4df1f65b99e7ca6e3f5
Rain <Valehtelit@mail.ru>
2026-01-14 22:40:59 +1000

    #По итогу готовы: идентификация, аутентификация, генерация токенов, проверки на ошибки

--- configs/routes.php
+++ configs/routes.php
@@ -1,18 +1,14 @@
 <?php
 #СБОР ОТЗЫВОВ В ПРИЛОЖЕНИИ
-$this->get('app/employee/get', 'Application\Employee\Controllers\EmployeeController::getEmployee')
-    ->middleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
-$this->post('app/review/add/positive', 'Application\ReviewCollector\Controllers\ReviewCollectorController::addReview')
-    ->middleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
+$this->get('app/employee/get', 'Application\Employee\Controllers\EmployeeController::getEmployee');
+$this->post('app/review/add/positive', 'Application\ReviewCollector\Controllers\ReviewCollectorController::addReview');
 
 #СБОР ДАННЫХ ДЛЯ ГЛАВНОЙ СТРАНИЦЫ ДАШБОРДА
-$this->get('dashboard/main/employees', 'Application\Dashboard\Controllers\DashboardMainController::showEmployeesWithRating')
-    ->lazyMiddleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
+$this->get('dashboard/main/employees', 'Application\Dashboard\Controllers\DashboardMainController::showEmployeesWithRating');
+    //->lazyMiddleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
 #СБОР ДАННЫХ ДЛЯ СТРАНИЦЫ ОТЗЫВОВ
-$this->get('dashboard/reviews/positive', 'Application\Dashboard\Controllers\DashboardReviewsController::showPositiveReviewsByEmployee')
-    ->middleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
-$this->get('dashboard/reviews/negative', 'Application\Dashboard\Controllers\DashboardReviewsController::showNegativeReviewsByEmployee')
-    ->middleware(\Engine\Auth\Authorization\AuthorizationMiddleware::class);
+$this->get('dashboard/reviews/positive', 'Application\Dashboard\Controllers\DashboardReviewsController::showPositiveReviewsByEmployee');
+$this->get('dashboard/reviews/negative', 'Application\Dashboard\Controllers\DashboardReviewsController::showNegativeReviewsByEmployee');
 #АВТОРИЗАЦИЯ
 $this->post('auth/doAuth', 'Application\Auth\Controllers\AuthController::auth')
     ->lazyMiddlewares([
